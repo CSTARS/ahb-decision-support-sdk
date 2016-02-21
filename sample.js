282235c6c1a9bff061969b907739fd9cfc6d82ad
Justin Merz <jrmerz@gmail.com>
2016-02-21 10:07:46 -0800

    adding random parcel selection, fix for multipolygons

--- sample.js
+++ sample.js
@@ -4,9 +4,13 @@ var ll = [40.600445, -122.204414];
 var radius = 5000;
 
 var t = new Date().getTime();
+var rand = 0.5;
 
 console.log('Finding parcels...');
 sdk.datastore.getParcels(ll[0], ll[1], radius, function(err, parcels){
+
+  sdk.datastore.randomizeSelected(rand);
+
   console.log('Retrieving weather...');
   sdk.datastore.getWeather(function(weather){
     console.log('Retrieving soil...\n');
@@ -16,7 +20,8 @@ sdk.datastore.getParcels(ll[0], ll[1], radius, function(err, parcels){
       console.log('Loading farm budget for poplar...\n');
       sdk.budget.load(function(){
         sdk.model.growAll();
-        console.log('\nDone. '+sdk.datastore.parcels.length+' parcels. '+(new Date().getTime()-t)+'ms');
+        console.log('\nDone. '+sdk.datastore.selectedParcels.length+' parcels. '+
+          (rand*100)+'% random sample. '+(new Date().getTime()-t)+'ms');
       });
     });
   });
